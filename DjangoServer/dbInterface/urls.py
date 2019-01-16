5ce26278561f524c86173649ebf54191e23f2dd8
LAPTOP-VBBRV88P\Nathan <nathanmoses13@gmail.com>
2019-01-16 13:52:35 +0000

    changed slashes to dashes in urls

--- DjangoServer/dbInterface/urls.py
+++ DjangoServer/dbInterface/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('save_log', views.save_log, name='save log'),
+    path('save-log', views.save_log, name='save log'),
 	path('bldg/<slug:id>/', views.bldg, name='bldg'),
 	path('bldg/<slug:name>/', views.bldg, name='get bldg by name')
-	path('add_bldg', views.add_bldg, name='add bldg'),
-	path('add_room', views.add_room, name='add room'),
-	path('add_sensor', views.add_sensor, name='add sensor'),
-	path('get_all_rooms', views.get_all_rooms, name='get all rooms'),
-    path('get_all_rooms_logs', views.get_all_rooms_logs, name=
+	path('add-bldg', views.add_bldg, name='add bldg'),
+	path('add-room', views.add_room, name='add room'),
+	path('add-sensor', views.add_sensor, name='add sensor'),
+	path('get-all-rooms', views.get_all_rooms, name='get all rooms'),
+    path('get-all-rooms-logs', views.get_all_rooms_logs, name='get all rooms logs'),
 	
 ]
