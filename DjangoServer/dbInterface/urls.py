fd8ca2de87ff204cb33b28a212758e94f741d226
LAPTOP-VBBRV88P\Nathan <nathanmoses13@gmail.com>
2019-01-21 21:27:50 +0000

    added few more urls need review

--- DjangoServer/dbInterface/urls.py
+++ DjangoServer/dbInterface/urls.py
@@ -11,5 +11,13 @@ urlpatterns = [
 	path('add-sensor', views.add_sensor, name='add sensor'),
 	path('get-all-rooms', views.get_all_rooms, name='get all rooms'),
     path('get-all-rooms-logs', views.get_all_rooms_logs, name='get all rooms logs'),
+	path('room/<slug:name>/', views.room, name='room'),
+	path('room/get-room-logs/', views.get_room_logs, name='get room logs'),
+	path('bldg/<slug:id>/get-rooms-by-bldg', views.get_rooms_by_bldg, name='get rooms by bldg'),
+	path('room/<slug:name>/get-sensors-in-room/', views.get_sensors_in_room, name='get sensors in room'),
+	path('log', views.log, name='log'),
+	path('save-logs', views.save_log, name='save logs'),
+	path('sensor/<slug:id>/', views.sensor, name='sensor'),
+	
 	
 ]
